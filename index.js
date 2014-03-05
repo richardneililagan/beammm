15cece5eefb8781f179bab623136ba27aff28eb8
Richard Neil Ilagan <richard.ilagan@hp.com>
2014-03-05 22:46:04 +0800

    LOL IP address.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var server = require('./app/server');
 server.init(
     process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1
+    process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
     );
