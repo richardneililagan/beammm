751457e87d3c4a4cacfe79d2cd6fbe40dc4d0d96
Richard Neil Ilagan <richard.ilagan@hp.com>
2014-03-05 23:02:14 +0800

    More debug fixing

--- app/loader-clean.js
+++ app/loader-clean.js
@@ -30,13 +30,16 @@ var Loader = function () {
 
             if (err) { console.log(err); }
 
+            console.log(body);
+
             // TODO error handling
-            var dom = cheerio.load(body);
-            console.log('dom initialized', dom);
+            var dom = cheerio.load(body)('body');
+            console.log('dom initialized', dom.html());
 
             if (!!selector && _.isString(selector)) {
                 console.log('using selector', selector);
-                dom = dom(selector);
+                dom = dom.find(selector);
+                console.log('reduced to', dom.html());
             }
 
             self.emit('loaded', dom);
